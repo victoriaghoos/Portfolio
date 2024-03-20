3c9bc6636732a39c95bd8c645f61f56a47672f85
victoriaghoos <victoriamarinus@gmail.com>
2024-03-20 14:21:19 +0100

    Add some basic layout to navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Contact">Contact</Link></li>
-        <li><Link to="/Skills">Skills</Link></li>
-      </ul>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <span className="text-white text-lg font-bold">My Portfolio</span>
+        <ul className="flex space-x-4">
+          <li><a href="#home" className="text-white hover:text-gray-300">Home</a></li>
+          <li><a href="#about" className="text-white hover:text-gray-300">About</a></li>
+          <li><a href="#skills" className="text-white hover:text-gray-300">Skills</a></li>
+          <li><a href="#contact" className="text-white hover:text-gray-300">Contact</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
