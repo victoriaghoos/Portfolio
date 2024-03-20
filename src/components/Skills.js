961c3bea29fc77d57aa2f53a62c6810a39f01fbf
victoriaghoos <victoriamarinus@gmail.com>
2024-03-20 14:25:48 +0100

    Initialize the remaining component files

---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <h1>Skills Page</h1>
+      {/* Add content here */}
+    </div>
+  );
+}
+
+export default Skills;
